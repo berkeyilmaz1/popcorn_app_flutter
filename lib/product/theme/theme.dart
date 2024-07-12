99437e7c1a9d098e442ff4cd7da6457009be9ff3
berkeyilmaz1 <berkeyilmaz189@gmail.com>
2024-07-12 10:49:18 +0300

    theme created

---
+++ lib/product/theme/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get appTheme {
+    return ThemeData(
+        fontFamily: "Raleway",
+        iconTheme: const IconThemeData(color: Colors.white),
+        elevatedButtonTheme: const ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                foregroundColor: WidgetStatePropertyAll(Colors.white))),
+        colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            primary: Colors.blue,
+            onPrimary: Colors.white,
+            secondary: Colors.lightBlue,
+            onSecondary: Colors.white,
+            error: Colors.red,
+            onError: Colors.white,
+            surface: Colors.black,
+            onSurface: Colors.white));
+  }
+}
