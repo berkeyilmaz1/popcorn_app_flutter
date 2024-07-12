8e1c2280e8a47662a7ad203f2a3753b77ab49079
berkeyilmaz1 <berkeyilmaz189@gmail.com>
2024-07-12 10:50:11 +0300

    padding extensions

---
+++ lib/product/extensions/extensions.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ProjectPadding extends EdgeInsets {
+  const ProjectPadding.all() : super.all(12);
+  const ProjectPadding.bigAll() : super.all(24);
+  const ProjectPadding.halfAll() : super.all(6);
+}
